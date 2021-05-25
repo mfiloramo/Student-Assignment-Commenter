3ba1874261ae2c3bf02b103590113c96b4814d8b
mfiloramo <mlfiloramo@gmail.com>
2021-05-25 01:13:04 -0400

    Code tweaks.

--- main.py
+++ main.py
@@ -35,28 +35,21 @@ frame.geometry('600x400')
 # Define interface buttons/text boxes
 inputtxt = Text(frame, height=1, width=15)
 nameButton = Button(frame, text="Input Name", command=define_name)
-# citeButton_glow = Button(frame, text="Citing Evidence", command=lambda: create_glow("Citing Evidence"))
-# citeButton_grow = Button(frame, text="Citing Evidence", command=lambda: create_grow("Citing Evidence"))
-# orgButton_glow = Button(frame, text="Organizing Handwriting", command=lambda: create_glow("Organizing Handwriting"))
-# orgButton_grow = Button(frame, text="Organizing Handwriting", command=lambda: create_grow("Organizing Handwriting"))
 
-test_dicts = {}
+glow_dict = {}
+grow_dict = {}
 
-# Build a set of buttons.
-glow_buttons = []
-grow_buttons = []
-for a, b in zip(sorted(skills_dict.keys()), range(1, 17)):
-    test_dicts[b] = Button(frame, text=a, command=lambda: create_glow(a))
+for a, b in zip(skills_dict.keys(), range(1, 17)):
+    glow_dict[b] = Button(frame, text=a, command=lambda: create_glow(a))
+    grow_dict[b] = Button(frame, text=a, command=lambda: create_grow(a))
 
+# Position the buttons within the GUI.
 list_rows = (1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4)
 list_cols = (1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4)
 
-for x, y, z in zip(test_dicts.values(), list_rows, list_cols):
-    x.grid(row=y, column=z)
-
-# for w, x, y, z in zip(glow_buttons, grow_buttons, list_rows, list_cols):
-#     w.grid(row=y, column=z, padx=2, pady=5)
-#     x.grid(row=y+9, column=z, padx=2, pady=5)
+for w, x, y, z in zip(glow_dict.values(), grow_dict.values(), list_rows, list_cols):
+    w.grid(row=y, column=z)
+    x.grid(row=y+8, column=z)
 
 commentButton = Button(frame, text="Generate Comment", command=generate_comment)
 
@@ -68,17 +61,9 @@ nameButton.grid(row=0, column=4)
 # glowLabel = Label(frame, text="Glows")
 # glowLabel.grid(row=4, column=3, pady=10)
 
-# Grid-align "Glow" section buttons.
-# citeButton_glow.grid(row=5, column=1, padx=10, pady=10)
-# orgButton_glow.grid(row=5, column=2)
-
 # Grid-align "Grow" section label
 # growLabel = Label(frame, text="Grows")
 # growLabel.grid(row=6, column=3, pady=10)
-
-# Grid-align "Grow" section buttons.
-# orgButton_grow.grid(row=7, column=2)
-# citeButton_grow.grid(row=7, column=1)
 
 # Grid-align generate_comment button.
 commentButton.grid(row=14, column=2, pady=10)
