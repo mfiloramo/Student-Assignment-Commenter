3c7c334da1c5aea25bfbdbcc0ad386f5fc9379ea
mfiloramo <mlfiloramo@gmail.com>
2021-05-24 20:33:02 -0400

    Further built button organization.

--- corpus.py
+++ corpus.py
@@ -5,6 +5,6 @@ grows_effort = ['try to work on', 'try paying more attention to']
 
 # Each key will be associated with a choice within a GUI.
 dict = {
-    'citing evidence': ['citing evidence from the text', 'citing text evidence', 'using text evidence'],
-    'organizing handwriting': ['making your handwriting organized', 'organizing your handwriting'],
+    'Citing Evidence': ['citing evidence from the text', 'citing text evidence', 'using text evidence'],
+    'Organizing Handwriting': ['making your handwriting organized', 'organizing your handwriting'],
 }
--- main.py
+++ main.py
@@ -28,29 +28,40 @@ def generate_comment():
 
 # Top level parent window
 frame = Tk()
-frame.title("TextBox Input")
-frame.geometry('400x200')
+frame.title("Assignment Commenter")
+frame.geometry('600x400')
 
 # Define interface buttons/text boxes
-inputtxt = Text(frame, height=2, width=7)
+inputtxt = Text(frame, height=1, width=15)
 nameButton = Button(frame, text="Input Name", command=define_name)
-citeButton_glow = Button(frame, text="citing evidence", command=lambda: create_glow("citing evidence"))
-citeButton_grow = Button(frame, text="citing evidence", command=lambda: create_grow("citing evidence"))
-orgButton_glow = Button(frame, text="organizing handwriting", command=lambda: create_glow("organizing handwriting"))
-orgButton_grow = Button(frame, text="organizing handwriting", command=lambda: create_grow("organizing handwriting"))
+citeButton_glow = Button(frame, text="Citing Evidence", command=lambda: create_glow("Citing Evidence"))
+citeButton_grow = Button(frame, text="Citing Evidence", command=lambda: create_grow("Citing Evidence"))
+orgButton_glow = Button(frame, text="Organizing Handwriting", command=lambda: create_glow("Organizing Handwriting"))
+orgButton_grow = Button(frame, text="Organizing Handwriting", command=lambda: create_grow("Organizing Handwriting"))
 commentButton = Button(frame, text="Generate Comment", command=generate_comment)
 
-# Pack interface buttons/text boxes
-inputtxt.grid(row=0, column=2)
-nameButton.grid(row=1, column=2)
+# Grid-align text input and define_name button.
+inputtxt.grid(row=0, column=3)
+nameButton.grid(row=1, column=3)
 
-citeButton_glow.grid(row=3, column=1, padx=10, pady=10)
-citeButton_grow.grid(row=4, column=1)
-orgButton_glow.grid(row=3, column=2)
-orgButton_grow.grid(row=4, column=2)
+# Grid-align Glow Section label.
+glowLabel = Label(frame, text="Glows")
+glowLabel.grid(row=4, column=3, pady=10)
 
+# Grid-align "Glow" section buttons.
+citeButton_glow.grid(row=5, column=1, padx=10, pady=10)
+orgButton_glow.grid(row=5, column=2)
 
-commentButton.grid(row=5, column=2, pady=10)
+# Grid-align "Grow" section label
+growLabel = Label(frame, text="Grows")
+growLabel.grid(row=6, column=3, pady=10)
+
+# Grid-align "Grow" section buttons.
+orgButton_grow.grid(row=7, column=2)
+citeButton_grow.grid(row=7, column=1)
+
+# Grid-align generate_comment button.
+commentButton.grid(row=8, column=3, pady=10)
 
 # Run the application.
 frame.mainloop()
@@ -61,6 +72,8 @@ frame.mainloop()
 # TODO: Develop corpus.
 
 # TODO: Create text output for copying and pasting.
+
+# TODO: Reorganize file structure.
 
 # TODO: Incorporate functionality with the Google Classroom API.
 
